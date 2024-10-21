226eb679a6a9dd48d493895f1234dc3fb5894bf1
tomkoom <tomash.sugint@gmail.com>
2024-10-21 10:56:07 +0700

    add copy-btn, update styles, update title

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,8 @@ import { FC, useState } from "react";
 import stc from "string-to-color";
 
 const App: FC = (): JSX.Element => {
-  const [string, setString] = useState<string>("");
+  const [string, setString] = useState<string>("Paste or input string");
+  const [copied, setCopied] = useState<boolean>(false);
   const color = stc(string);
 
   const clear = (): void => {
@@ -13,11 +14,17 @@ const App: FC = (): JSX.Element => {
     navigator.clipboard.readText().then((s) => setString(s));
   };
 
+  const copy = (): void => {
+    setCopied(true);
+    navigator.clipboard.writeText(color);
+    setTimeout(() => setCopied(false), 1_000);
+  };
+
   return (
-    <div className="font-press-start-2p text-gray-50">
+    <div className="font-press-start-2p text-white">
       <div className="flex items-center overflow-x-auto">
         <input
-          className="w-full text-gray-950 min-w-[160px] h-[50px] px-4 text-lg font-bold outline-none caret-shape placeholder:text-gray-400"
+          className="w-full bg-white text-gray-950 min-w-[160px] h-[50px] px-4 text-lg font-bold outline-none caret-shape placeholder:text-gray-400"
           type="text"
           placeholder="Paste or input string"
           value={string}
@@ -26,16 +33,16 @@ const App: FC = (): JSX.Element => {
 
         <div className="flex items-center text-base font-black">
           <button
-            className="h-[50px] bg-gray-950 px-8 transition-colors hover:bg-gray-800"
+            className="h-[48px] bg-gray-950 px-8 transition-colors hover:bg-gray-800"
             onClick={clear}
           >
             Clear
           </button>
 
-          <div className="h-[50px] w-[2px] bg-gray-800"></div>
+          <div className="h-[50px] w-px bg-white"></div>
 
           <button
-            className="h-[50px] bg-gray-950 px-8 transition-colors hover:bg-gray-800"
+            className="h-[48px] bg-gray-950 px-8 transition-colors hover:bg-gray-800 mr-px"
             onClick={paste}
           >
             Paste
@@ -46,7 +53,15 @@ const App: FC = (): JSX.Element => {
         style={{ backgroundColor: color }}
         className="flex items-center justify-center h-[calc(100vh-50px)]"
       >
-        <span className="text-2xl font-black">{color}</span>
+        <div className="flex flex-col items-center gap-2">
+          <span className="text-2xl font-black">{color}</span>
+          <button
+            className="h-[50px] bg-gray-950 px-8 transition-colors hover:bg-gray-800"
+            onClick={copy}
+          >
+            {copied ? "Copied!" : "Copy Color"}
+          </button>
+        </div>
       </div>
     </div>
   );
